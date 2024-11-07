a252942597c34ec01bd0fd61f67d44c6a3657db4
mahdisaljoughi <saljooghimahdii@gmail.com>
2024-11-07 15:44:59 +0330

    fix

--- src/app/dashboard/shop-management/page.tsx
+++
@@ -1,9 +0,0 @@
-import ManagementShop from "@/components/Dashboard/ShopManagement/ShopManagement";
-
-export default function Page() {
-  return (
-    <>
-      <ManagementShop />
-    </>
-  );
-}
--- src/components/Dashboard/ShopManagement/ShopManagement.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ShopManagement() {
-  return (
-    <>
-      <div className="min-h-screen bg-profile-gradient">ShopManagement</div>
-    </>
-  );
-}
